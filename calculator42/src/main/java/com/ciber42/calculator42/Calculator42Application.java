fe8e8e9d5dff9f2eff753fa7fb61996ba10a03d3
northern98 <jorgeaga75@educastur.es>
2022-05-13 17:51:47 +0200

    Añadido esqueleto Spring Boot

---
+++ calculator42/src/main/java/com/ciber42/calculator42/Calculator42Application.java
@@ -0,0 +1,13 @@
+package com.ciber42.calculator42;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator42Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator42Application.class, args);
+	}
+
+}
---
+++ calculator42/src/test/java/com/ciber42/calculator42/Calculator42ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciber42.calculator42;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator42ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
